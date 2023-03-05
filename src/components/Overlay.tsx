17db1011665a036c43453d6df543bc993ff7e9ea
xWxfFle <factores175@gmail.com>
2023-03-06 00:02:43 +0300

    Added new text to overlay

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,17 +1,16 @@
+import { useProgress } from "@react-three/drei";
+
 export const Overlay = () => {
+  const { progress } = useProgress();
   return (
     <div
       className={
         "absolute top-0 left-0 w-full h-screen flex bg-zinc-900 flex-col justify-center items-center text-zinc-100 text-xl"
       }
     >
-      <div className="border-l-4 border-sky-900 pl-2">
-        <p>You need a mouse and keyboard to test this demo </p>
-        <p>
-          Use WASD or Arrows for movement <br />
-          Use Space to jump
-        </p>
-        <br />
+      <h1 className=" text-2xl">loading... {progress.toFixed()} % </h1>
+    
+      <div className="border-t-4 border-sky-900 pt-2">
         <p>
           GitHub: xWxfFle <br />
           Telegram: @xWxfFle
